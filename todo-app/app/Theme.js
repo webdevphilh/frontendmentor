2f4abe8f16cd306210e58cd7cf18c95abbf6870b
phil <philiphargens@gmail.com>
2021-03-24 18:48:15 +0100

    design mostly done

--- todo-app/app/Theme.js
+++ todo-app/app/Theme.js
@@ -1,6 +1,4 @@
 
-
-
 const html = document.querySelector('html');
 html.dataset.theme = `theme-light`;
 
@@ -9,14 +7,11 @@ function switchTheme() {
   if ( html.dataset.theme === `theme-light`){
     themeSwitch.src = "./images/icon-sun.svg";
     html.dataset.theme = `theme-dark`;
-    // mobile vs desktop einstellen
-    document.body.style.setProperty("background-image", "url(\"../images/bg-mobile-dark.jpg\")");
   }
 
   else{
     themeSwitch.src = "./images/icon-moon.svg";
     html.dataset.theme = `theme-light`;
-    document.body.style.setProperty("background-image", "url(\"../images/bg-mobile-light.jpg\")");
     }
   console.log(html.dataset.theme);
 }
